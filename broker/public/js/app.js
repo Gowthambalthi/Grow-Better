d1458665550b68143e7c55eb9000219f4a447fe3
Gowthambalthi <gowthm.balthi0@gmail.com>
2026-08-21 02:18:38 +0530

    style: rename 10th column header to DAYS HOLD

--- broker/public/js/app.js
+++ broker/public/js/app.js
@@ -3,15 +3,15 @@
  * Main Application Bootstrapper & ES Module Entry Point
  */
 
-import { initTheme } from './core/theme.js?v=20260821_v102';
-import { initRouter } from './core/router.js?v=20260821_v102';
-import { initPopovers, startTicker } from './components/ticker.js?v=20260821_v102';
-import { loadStatus } from './components/statusPill.js?v=20260821_v102';
-import { initPortfolio, loadPortfolio } from './modules/portfolio.js?v=20260821_v102';
-import { initOrders } from './modules/orders.js?v=20260821_v102';
-import { loadMtfPositions, initPositionsSubtabs } from './modules/positions.js?v=20260821_v102';
-import { initInstitutionalScanner } from './modules/scanner.js?v=20260821_v102';
-import { initSettingsView, loadFundsTotals } from './modules/settings.js?v=20260821_v102';
+import { initTheme } from './core/theme.js?v=20260821_v103';
+import { initRouter } from './core/router.js?v=20260821_v103';
+import { initPopovers, startTicker } from './components/ticker.js?v=20260821_v103';
+import { loadStatus } from './components/statusPill.js?v=20260821_v103';
+import { initPortfolio, loadPortfolio } from './modules/portfolio.js?v=20260821_v103';
+import { initOrders } from './modules/orders.js?v=20260821_v103';
+import { loadMtfPositions, initPositionsSubtabs } from './modules/positions.js?v=20260821_v103';
+import { initInstitutionalScanner } from './modules/scanner.js?v=20260821_v103';
+import { initSettingsView, loadFundsTotals } from './modules/settings.js?v=20260821_v103';
 
 function refreshAll() {
   loadStatus();
